0183b61b6f17fa6c0e2a00884ab51a28d6e125f8
Ben Spotswood <spotswoodb@gmail.com>
2021-05-22 12:52:43 -0400

    Done.

--- index.js
+++ index.js
@@ -10,4 +10,23 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+
+function greet (timeStr) {
+  let nowTime = timeStr.split(':');
+  let nowHour = parseInt(nowTime[0]);
+
+  if (nowHour<12) {
+    return 'Good Morning';
+  }
+  else if (nowHour>17) {
+    return 'Good Evening';
+  }
+  return 'Good Afternoon';
+}
+
+
 /* Write your implementation of displayMessage() */
+
+function displayMessage(msg) {
+  document.getElementById('greeting').innerText = msg;
+}
