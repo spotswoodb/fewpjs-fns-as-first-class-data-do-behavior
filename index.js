db08f98b711be69e5afc2deca554a1c33ce31e5d
Ben Spotswood <spotswoodb@gmail.com>
2021-08-25 22:41:38 -0500

    commit tree e44ea2eeb1490dc787d5a176fb76d4177b80dbe7

--- index.js
+++ index.js
@@ -11,22 +11,27 @@ function handleClick(e) {
 
 /* Write your implementation of greet() */
 
-function greet (timeStr) {
+
+function greet(timeStr){
   let nowTime = timeStr.split(':');
   let nowHour = parseInt(nowTime[0]);
 
-  if (nowHour<12) {
-    return 'Good Morning';
+  // what dont I understand about this^ nothing. Idk why I was trying to call a method on now Time.
+  // I'm using that in the argument of parseInt
+  if (nowHour < 12){
+    return "Good Morning";
   }
-  else if (nowHour>17) {
-    return 'Good Evening';
+
+  else if (nowHour > 17){
+    return "Good Evening";
   }
-  return 'Good Afternoon';
+
+  return "Good Afternoon";
+}
+
+function displayMessage(msg){
+  document.getElementById('greeting').innerText = msg;
 }
 
 
 /* Write your implementation of displayMessage() */
-
-function displayMessage(msg) {
-  document.getElementById('greeting').innerText = msg;
-}
